e700074532c0ee8403fbc5c4b435a2a4cc254c40
kstark1 <k.stark1478@gmai.com>
2019-07-09 15:48:30 -0600

    code for modifying attribute, adding element and writing to file

--- src/com/company/editAnvilFile.java
+++ src/com/company/editAnvilFile.java
@@ -2,6 +2,7 @@ package com.company;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -22,6 +23,33 @@ public class editAnvilFile {
             doc.getDocumentElement().normalize(); // removes empty text nodes and joins adjacent nodes
 
 
+            // adding a new element
+            NodeList trackLi = doc.getElementsByTagName("track");
+           // System.out.println(trackLi.getLength());
+            NodeList wordsLi = ((Element)trackLi.item(0)).getElementsByTagName("el");
+
+            Element wordsEl = (Element) trackLi.item(0);
+
+           /* Element el = doc.createElement("el");
+            el.setAttribute("index", "1");
+            el.setAttribute("start", "30");
+            el.setAttribute("end", "45"); */
+
+           //wordsEl.appendChild(el);
+
+
+
+
+
+/*
+            // changing the source of the video, initially using video from downloads file
+            NodeList videoLi = doc.getElementsByTagName("video");
+            Element video = (Element) videoLi.item(0);
+            System.out.println(video.getAttribute("src"));
+            video.setAttribute("src", "smallTP (1).avi");
+            System.out.println(video.getAttribute("src"));
+
+
             NodeList trackLi = doc.getElementsByTagName("track");
 
             System.out.println(trackLi.getLength());
@@ -40,20 +68,14 @@ public class editAnvilFile {
 
             wordsEl.setAttribute("start",var.toString());
             wordsEl.setAttribute("end", varEnd.toString());
-            System.out.println(wordsEl.getAttribute("start"));
+            System.out.println(wordsEl.getAttribute("start")); */
 
+/*
             // Writes the information into a new file
             Transformer tf = TransformerFactory.newInstance().newTransformer();
             DOMSource domSource = new DOMSource(doc);
             StreamResult sr = new StreamResult("twoPeople.anvil");
-            tf.transform(domSource, sr);
-
-/*
-            TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Transformer transformer = transformerFactory.newTransformer();
-            DOMSource source = new DOMSource(doc);
-            StreamResult consoleResult = new StreamResult(System.out);
-            transformer.transform(source, consoleResult);*/
+            tf.transform(domSource, sr); */
 
 
 
